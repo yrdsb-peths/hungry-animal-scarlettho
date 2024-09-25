0246e71a180dcb1a2c62ca912c464147327061a1
scarlettho <159584626+scarlettho@users.noreply.github.com>
2024-09-24 22:01:04 -0400

    Adjusting texts

--- TitleScreen.java
+++ TitleScreen.java
@@ -17,9 +17,11 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+
+        addObject(titleLabel, getWidth()/2 + 45, getHeight()/2);
+        prepare();
     }
+
     /**
      * The main vorld act loop
      */
@@ -31,5 +33,22 @@ public class TitleScreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Seal seal = new Seal();
+        addObject(seal,47,186);
+        seal.setLocation(72,200);
+        Label label = new Label("Press <space> to Start", 40);
+        addObject(label,266,231);
+        label.setLocation(300,357);
+        Label label2 = new Label("Use <-- and --> to move", 35);
+        addObject(label2,94,286);
+        label2.setLocation(300,36);
     }
 }
